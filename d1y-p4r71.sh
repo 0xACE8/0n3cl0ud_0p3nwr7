66afc02d78241bd998451d6d95eceab42db3af0e
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-10-12 08:59:44 -0700

    Update d1y-p4r71.sh

--- d1y-p4r71.sh
+++ d1y-p4r71.sh
@@ -2,5 +2,5 @@ sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463;main' feeds.conf
 sed -i '2 i\src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
 sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' feeds.conf.default
 #sed -i '$a src-git quickstart https://github.com/0xACE8/openwrt-quickstart' feeds.conf.default
-sed -i '$a src-git xd https://github.com/shiyu1314/onecloud-package' feeds.conf.default
+#sed -i '$a src-git xd https://github.com/shiyu1314/onecloud-package' feeds.conf.default
 svn co https://github.com/0xACE8/0n3cl0ud_0p3nwr7/trunk/target/linux/meson target/linux/meson
