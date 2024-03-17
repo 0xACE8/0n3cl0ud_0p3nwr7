3c22a29a097cd4fdbe4216891e24ca98475cf954
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-03-17 02:09:05 -0700

    Update d1y-p4r71.sh

--- d1y-p4r71.sh
+++ d1y-p4r71.sh
@@ -14,11 +14,8 @@ git_sparse_clone main https://github.com/shiyu1314/openwrt-onecloud device
 cp -r device/* ./
 rm -rf device
 
-
 sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463;0n3cl0ud' feeds.conf.default
-sed -i '2 i\src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' feeds.conf.default
-#sed -i '$a src-git quickstart https://github.com/0xACE8/openwrt-quickstart' feeds.conf.default
+sed -i '2 i\src-git mosdns https://github.com/sbwml/luci-app-mosdns' feeds.conf.default
+sed -i '3 i\src-git openwrt_helloworld https://github.com/sbwml/openwrt_helloworld' feeds.conf.default
 #sed -i '$a src-git xd https://github.com/shiyu1314/onecloud-package' feeds.conf.default
-sed -i '$a src-git mosdns https://github.com/0xACE8/luci-app-mosdns' feeds.conf.default
 
